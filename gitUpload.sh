d5b39f0837e7b1c6dd60bd4c2615e83c5717474a
mymacy <marcelalbrink@googlemail.com>
2018-06-30 02:52:11 +0200

    first commit

--- gitUpload.sh
+++ gitUpload.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 clear
+echo "uploading repository"
+
+#git init 
+git add .
 git commit -m "first commit"
 git remote add origin https://github.com/mymacy/quasar-website-template.git
 gut push -u origin master
+
+echo "done"
